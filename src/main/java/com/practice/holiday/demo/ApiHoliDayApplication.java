88fc36c2d24c9479a0f42e098916feb07f2a178a
Algaal <alejandro.gallego5@udea.edu.co>
2023-12-02 18:32:52 -0500

    Front

---
+++ src/main/java/com/practice/holiday/demo/ApiHoliDayApplication.java
@@ -0,0 +1,13 @@
+package com.practice.holiday.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHoliDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHoliDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/holiday/demo/ApiHoliDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.holiday.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHoliDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
